7da7f7c7da54c5a1d60fd8b89e04d90062dbcc89
Chandrikslll <132633334+Chandrikslll@users.noreply.github.com>
2023-06-11 07:30:08 +0530

    Add files via upload

---
+++ e -learning web page/product landing page/quiz using html,css,js/script.js
@@ -0,0 +1,91 @@
+const quizData = [
+    {
+        question: "Who is the first president of india?",
+        a: "Dr.Br Ambedkar",
+        b: "Jawaharlal Nehru",
+        c: "Sardar vallabhbhai patel",
+        d: "Dr.Rajendra Prasad",
+        correct: "d",
+    },
+    {
+        question: "Name the country which has no capital?",
+        a: "India",
+        b: "Nauru",
+        c: "USA",
+        d: "China",
+        correct: "b",
+    },
+    {
+        question: "Which Indian Airport uses only solar power to run its operations?",
+        a: "Cochin Inernational Airport",
+        b: "Indra Gandhi International Airport",
+        c: "Chhtrapati Shivaji Mahraj International Airport",
+        d: "None of the Above",
+        correct: "a",
+    },
+    {
+        question: " Which is the Largest Dam in India?",
+        a: "Bhakra Dam",
+        b: "Tehri Dam",
+        c: "Nagarjun Sagar Dam",
+        d: "none of the above",
+        correct: "b",
+    },
+    {
+        question: " Which city is called Pearl city of India?",
+        a: "Chennai",
+        b: "Bengaluru",
+        c: "Hyderabad",
+        d: "none of the above",
+        correct: "c",
+    },
+];
+const quiz= document.getElementById('quiz')
+const answerEls = document.querySelectorAll('.answer')
+const questionEl = document.getElementById('question')
+const a_text = document.getElementById('a_text')
+const b_text = document.getElementById('b_text')
+const c_text = document.getElementById('c_text')
+const d_text = document.getElementById('d_text')
+const submitBtn = document.getElementById('submit')
+let currentQuiz = 0
+let score = 0
+loadQuiz()
+function loadQuiz() {
+    deselectAnswers()
+    const currentQuizData = quizData[currentQuiz]
+    questionEl.innerText = currentQuizData.question
+    a_text.innerText = currentQuizData.a
+    b_text.innerText = currentQuizData.b
+    c_text.innerText = currentQuizData.c
+    d_text.innerText = currentQuizData.d
+}
+function deselectAnswers() {
+    answerEls.forEach(answerEl => answerEl.checked = false)
+}
+function getSelected() {
+    let answer
+    answerEls.forEach(answerEl => {
+        if(answerEl.checked) {
+            answer = answerEl.id
+        }
+    })
+    return answer
+}
+submitBtn.addEventListener('click', () => {
+    const answer = getSelected()
+    if(answer) {
+       if(answer === quizData[currentQuiz].correct) {
+           score++
+       }
+       currentQuiz++
+       if(currentQuiz < quizData.length) {
+           loadQuiz()
+       } else {
+           quiz.innerHTML = `
+           <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+           <button onclick="location.reload()">Reload</button>
+           `
+       }
+    }
+})
